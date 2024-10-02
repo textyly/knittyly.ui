88daa7bc1f57358d3ce4739e430a0499e830dcc8
stefan <stefan.bozov@outlook.com>
2024-10-02 10:43:52 +0300

    add debug profile

--- scripts/copy.js
+++ scripts/copy.js
@@ -3,7 +3,10 @@ import fs from "fs-extra";
 try {
     console.log("start copying...");
 
-    console.log("copying html files to dist...");
+    console.log("copying index.html files to dist...");
+    fs.copyFileSync("./src/index.html", "./dist/index.html");
+
+    console.log("copying rest of the html files to dist...");
     fs.copySync("./src/html", "./dist/html");
 
     console.log("copying styles to dist...");
