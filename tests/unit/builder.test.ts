af5270a5b44d4b79f057d0b445f612a88114fda5
stefan <stefan.bozov@outlook.com>
2024-10-04 11:54:57 +0300

    better test description

--- tests/unit/builder.test.ts
+++ tests/unit/builder.test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { AppBuilder } from "../../src/code/builder.js";
 
 describe('app builder', () => {
-    it('build return app', () => {
+    it('return app must be defined', () => {
         const builder = new AppBuilder();
         const app = builder.build();
         expect(app).to.exist;
